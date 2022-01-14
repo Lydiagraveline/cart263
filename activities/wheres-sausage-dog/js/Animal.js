0fb0fc617472f9ca3b886d67aaf4cb59bf330976
lydia g <lydiag@lydias-Air.hsd1.md.comcast.net>
2022-01-14 16:00:33 -0500

    A1: added an update and display function to the animal class

--- activities/wheres-sausage-dog/js/Animal.js
+++ activities/wheres-sausage-dog/js/Animal.js
@@ -6,4 +6,19 @@ class Animal {
 
     this.angle = 0;
   }
+
+  // An update method that calls the display method
+  update(){
+    this.display();
+  }
+
+  // displays the animals
+  dsiplay() {
+    push();
+    imageMode(CENTER);
+    translate(this.x, this.y);
+    rotate(this.angle);
+    image(this.image, 0, 0);
+    pop();
+  }
 }
