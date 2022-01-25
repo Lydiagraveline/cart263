adf8139e80138f4c1717f2c5fdfd4e47fa7a8141
Lydia <ldgravy@gmail.com>
2022-01-25 11:49:53 -0500

    A2: added animal names array

--- activities/slamina/js/script.js
+++ activities/slamina/js/script.js
@@ -1,33 +1,149 @@
 /**
-Title of Project
-Author Name
+Slamina
+Lydia Graveline
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+A guessing game in which the page pronounces the name of an animal
+backwards and the user has to figure out what it was and say the
+name forwards.
 */
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
-function setup() {
-
-}
-
-
-/**
-Description of draw()
-*/
-function draw() {
-
-}
+// An array of animal names from
+// https://github.com/dariusk/corpora/blob/master/data/animals/common.json
+const animals = [
+      "aardvark",
+      "alligator",
+      "alpaca",
+      "antelope",
+      "ape",
+      "armadillo",
+      "baboon",
+      "badger",
+      "bat",
+      "bear",
+      "beaver",
+      "bison",
+      "boar",
+      "buffalo",
+      "bull",
+      "camel",
+      "canary",
+      "capybara",
+      "cat",
+      "chameleon",
+      "cheetah",
+      "chimpanzee",
+      "chinchilla",
+      "chipmunk",
+      "cougar",
+      "cow",
+      "coyote",
+      "crocodile",
+      "crow",
+      "deer",
+      "dingo",
+      "dog",
+      "donkey",
+      "dromedary",
+      "elephant",
+      "elk",
+      "ewe",
+      "ferret",
+      "finch",
+      "fish",
+      "fox",
+      "frog",
+      "gazelle",
+      "gila monster",
+      "giraffe",
+      "gnu",
+      "goat",
+      "gopher",
+      "gorilla",
+      "grizzly bear",
+      "ground hog",
+      "guinea pig",
+      "hamster",
+      "hedgehog",
+      "hippopotamus",
+      "hog",
+      "horse",
+      "hyena",
+      "ibex",
+      "iguana",
+      "impala",
+      "jackal",
+      "jaguar",
+      "kangaroo",
+      "koala",
+      "lamb",
+      "lemur",
+      "leopard",
+      "lion",
+      "lizard",
+      "llama",
+      "lynx",
+      "mandrill",
+      "marmoset",
+      "mink",
+      "mole",
+      "mongoose",
+      "monkey",
+      "moose",
+      "mountain goat",
+      "mouse",
+      "mule",
+      "muskrat",
+      "mustang",
+      "mynah bird",
+      "newt",
+      "ocelot",
+      "opossum",
+      "orangutan",
+      "oryx",
+      "otter",
+      "ox",
+      "panda",
+      "panther",
+      "parakeet",
+      "parrot",
+      "pig",
+      "platypus",
+      "polar bear",
+      "porcupine",
+      "porpoise",
+      "prairie dog",
+      "puma",
+      "rabbit",
+      "raccoon",
+      "ram",
+      "rat",
+      "reindeer",
+      "reptile",
+      "rhinoceros",
+      "salamander",
+      "seal",
+      "sheep",
+      "shrew",
+      "silver fox",
+      "skunk",
+      "sloth",
+      "snake",
+      "squirrel",
+      "tapir",
+      "tiger",
+      "toad",
+      "turtle",
+      "walrus",
+      "warthog",
+      "weasel",
+      "whale",
+      "wildcat",
+      "wolf",
+      "wolverine",
+      "wombat",
+      "woodchuck",
+      "yak",
+      "zebra"
+    ];
