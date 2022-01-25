1ff51b210a7bfba51aa8f984db5370b7196bf23c
Lydia <ldgravy@gmail.com>
2022-01-25 14:06:30 -0500

    A2: added new commands

--- exercises/slamina/js/script.js
+++ exercises/slamina/js/script.js
@@ -11,142 +11,146 @@ name forwards.
 
 // An array of animal names from
 // https://github.com/dariusk/corpora/blob/master/data/animals/common.json
-const animals = [
-      "aardvark",
-      "alligator",
-      "alpaca",
-      "antelope",
-      "ape",
-      "armadillo",
-      "baboon",
-      "badger",
-      "bat",
-      "bear",
-      "beaver",
-      "bison",
-      "boar",
-      "buffalo",
-      "bull",
-      "camel",
-      "canary",
-      "capybara",
-      "cat",
-      "chameleon",
-      "cheetah",
-      "chimpanzee",
-      "chinchilla",
-      "chipmunk",
-      "cougar",
-      "cow",
-      "coyote",
-      "crocodile",
-      "crow",
-      "deer",
-      "dingo",
-      "dog",
-      "donkey",
-      "dromedary",
-      "elephant",
-      "elk",
-      "ewe",
-      "ferret",
-      "finch",
-      "fish",
-      "fox",
-      "frog",
-      "gazelle",
-      "gila monster",
-      "giraffe",
-      "gnu",
-      "goat",
-      "gopher",
-      "gorilla",
-      "grizzly bear",
-      "ground hog",
-      "guinea pig",
-      "hamster",
-      "hedgehog",
-      "hippopotamus",
-      "hog",
-      "horse",
-      "hyena",
-      "ibex",
-      "iguana",
-      "impala",
-      "jackal",
-      "jaguar",
-      "kangaroo",
-      "koala",
-      "lamb",
-      "lemur",
-      "leopard",
-      "lion",
-      "lizard",
-      "llama",
-      "lynx",
-      "mandrill",
-      "marmoset",
-      "mink",
-      "mole",
-      "mongoose",
-      "monkey",
-      "moose",
-      "mountain goat",
-      "mouse",
-      "mule",
-      "muskrat",
-      "mustang",
-      "mynah bird",
-      "newt",
-      "ocelot",
-      "opossum",
-      "orangutan",
-      "oryx",
-      "otter",
-      "ox",
-      "panda",
-      "panther",
-      "parakeet",
-      "parrot",
-      "pig",
-      "platypus",
-      "polar bear",
-      "porcupine",
-      "porpoise",
-      "prairie dog",
-      "puma",
-      "rabbit",
-      "raccoon",
-      "ram",
-      "rat",
-      "reindeer",
-      "reptile",
-      "rhinoceros",
-      "salamander",
-      "seal",
-      "sheep",
-      "shrew",
-      "silver fox",
-      "skunk",
-      "sloth",
-      "snake",
-      "squirrel",
-      "tapir",
-      "tiger",
-      "toad",
-      "turtle",
-      "walrus",
-      "warthog",
-      "weasel",
-      "whale",
-      "wildcat",
-      "wolf",
-      "wolverine",
-      "wombat",
-      "woodchuck",
-      "yak",
-      "zebra"
-    ];
+// const animals = [
+//       "aardvark",
+//       "alligator",
+//       "alpaca",
+//       "antelope",
+//       "ape",
+//       "armadillo",
+//       "baboon",
+//       "badger",
+//       "bat",
+//       "bear",
+//       "beaver",
+//       "bison",
+//       "boar",
+//       "buffalo",
+//       "bull",
+//       "camel",
+//       "canary",
+//       "capybara",
+//       "cat",
+//       "chameleon",
+//       "cheetah",
+//       "chimpanzee",
+//       "chinchilla",
+//       "chipmunk",
+//       "cougar",
+//       "cow",
+//       "coyote",
+//       "crocodile",
+//       "crow",
+//       "deer",
+//       "dingo",
+//       "dog",
+//       "donkey",
+//       "dromedary",
+//       "elephant",
+//       "elk",
+//       "ewe",
+//       "ferret",
+//       "finch",
+//       "fish",
+//       "fox",
+//       "frog",
+//       "gazelle",
+//       "gila monster",
+//       "giraffe",
+//       "gnu",
+//       "goat",
+//       "gopher",
+//       "gorilla",
+//       "grizzly bear",
+//       "ground hog",
+//       "guinea pig",
+//       "hamster",
+//       "hedgehog",
+//       "hippopotamus",
+//       "hog",
+//       "horse",
+//       "hyena",
+//       "ibex",
+//       "iguana",
+//       "impala",
+//       "jackal",
+//       "jaguar",
+//       "kangaroo",
+//       "koala",
+//       "lamb",
+//       "lemur",
+//       "leopard",
+//       "lion",
+//       "lizard",
+//       "llama",
+//       "lynx",
+//       "mandrill",
+//       "marmoset",
+//       "mink",
+//       "mole",
+//       "mongoose",
+//       "monkey",
+//       "moose",
+//       "mountain goat",
+//       "mouse",
+//       "mule",
+//       "muskrat",
+//       "mustang",
+//       "mynah bird",
+//       "newt",
+//       "ocelot",
+//       "opossum",
+//       "orangutan",
+//       "oryx",
+//       "otter",
+//       "ox",
+//       "panda",
+//       "panther",
+//       "parakeet",
+//       "parrot",
+//       "pig",
+//       "platypus",
+//       "polar bear",
+//       "porcupine",
+//       "porpoise",
+//       "prairie dog",
+//       "puma",
+//       "rabbit",
+//       "raccoon",
+//       "ram",
+//       "rat",
+//       "reindeer",
+//       "reptile",
+//       "rhinoceros",
+//       "salamander",
+//       "seal",
+//       "sheep",
+//       "shrew",
+//       "silver fox",
+//       "skunk",
+//       "sloth",
+//       "snake",
+//       "squirrel",
+//       "tapir",
+//       "tiger",
+//       "toad",
+//       "turtle",
+//       "walrus",
+//       "warthog",
+//       "weasel",
+//       "whale",
+//       "wildcat",
+//       "wolf",
+//       "wolverine",
+//       "wombat",
+//       "woodchuck",
+//       "yak",
+//       "zebra"
+//     ];
+
+const animals = [`test`];
+
+const QUESTION_DELAY = 2000; // in milliseconds
 
 // The current answer to display (used initially to display the instructions)
 let currentAnswer = `Click to begin.`;
@@ -161,20 +165,20 @@ Set text defaults
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // checks if annyang available
-  if (annyang){
+  if (annyang) {
     // Create the guessing command
     let commands = {
-      '*animal': guessAnimal
+      "(is it) *animal": guessAnimal,
+      "skip": nextQuestion,
+      "help": nextQuestion,
     };
     annyang.addCommands(commands);
     annyang.start();
   }
-
   // Default text
   textSize(100);
   textAlign(CENTER);
   textFont("courier");
-
 }
 
 /**
@@ -183,7 +187,7 @@ Display the current answer.
 function draw() {
   background(0);
 
-    displayAnswer();
+  displayAnswer();
 }
 
 /**
@@ -193,11 +197,33 @@ Display the current answer in red if incorrect and green if correct
 function displayAnswer() {
   if (currentAnswer === currentAnimal) {
     fill(0, 255, 0);
-  }
-  else {
+  } else {
     fill(255);
   }
   text(currentAnswer, width / 2, height / 2);
+  text(reverseString(currentAnimal), width / 2, height / 3);
+}
+
+/**
+Reverse the animal name and say it with ResponsiveVoice
+*/
+function sayAnimalBackwards(animal) {
+  let reverseAnimal = reverseString(animal);
+  responsiveVoice.speak(reverseAnimal, "Russian Female");
+}
+
+/**
+Reverses the provided string
+*/
+function reverseString(string) {
+  // Split the string into an array of characters
+  let characters = string.split("");
+  // Reverse the array of characters
+  let reverseCharacters = characters.reverse();
+  // Join the array of characters back into a string
+  let result = reverseCharacters.join("");
+  // Return the result
+  return result;
 }
 
 /**
@@ -211,35 +237,22 @@ function guessAnimal(animal) {
 }
 
 /**
-Reverses the provided string
-*/
-function reverseString(string) {
-  // Split the string into an array of characters
-  let characters = string.split('');
-  // Reverse the array of characters
-  let reverseCharacters = characters.reverse();
-  // Join the array of characters back into a string
-  let result = reverseCharacters.join('');
-  // Return the result
-  return result;
-}
-
-
-/**
 Reset the answer text, get a new random animal, say its name
 */
-function nextQuestion(){
-  currentAnswer = `...`;
+function nextQuestion() {
+  currentAnswer = `is it ___?`;
   currentAnimal = random(animals);
 
-  //Reverse the animal name and say it with ResponsiveVoice
-  let reverseAnimal = reverseString(currentAnimal);
-  responsiveVoice.speak(reverseAnimal);
+  sayAnimalBackwards(currentAnimal);
 }
 
 /**
-When the user clicks, go to the next question
+When the user clicks, go to the next question or repeat current animal
 */
 function mousePressed() {
-  nextQuestion()
+  if (currentAnswer === currentAnimal || currentAnimal === `...`) {
+    nextQuestion();
+  } else {
+    sayAnimalBackwards(currentAnimal);
+  }
 }
