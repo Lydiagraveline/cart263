87f557315ca9ff219a2f56e1e8582a1840de4956
Lydia <ldgravy@gmail.com>
2022-01-31 14:18:04 -0500

    A3: Program asks for the user’s name and displays a default profile

--- activities/spy-profile-generator/js/script.js
+++ activities/spy-profile-generator/js/script.js
@@ -1,13 +1,19 @@
 /**
-Title of Project
+Spy Profile activity
 Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+generates a randomized spy profile for the user, and password protects it
 */
 
 "use strict";
 
+// The spy profile data while the program is running
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  secretWeapons: `**REDACTED**`,
+  password: `**REDACTED**`,
+};
 
 /**
 Description of preload
@@ -21,13 +27,33 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  spyProfile.name = prompt(`What is your name?`);
 }
 
 
 /**
-Description of draw()
+Displays the current spy profile.
 */
 function draw() {
+  background(255, 20, 150);
+
+  // Generate the profile as a string using the data
+  let spyText = `** TOP SECRET SPY PROFILE **
+
+Name: ${spyProfile.name}
+Alias: ${spyProfile.alias}
+Secret Weapon: ${spyProfile.secretWeapon}
+Password: ${spyProfile.password}`;
+
+  // Display the profile
+  push();
+  textFont(`Courier, monospace`);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  fill(0);
+  text(spyText, 0, 0);
+  pop();
 
 }
