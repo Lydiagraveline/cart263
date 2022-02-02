3f3bb3a64039a879ecf893d50bcffc1d7df6e1e7
Lydia <ldgravy@gmail.com>
2022-02-01 19:31:38 -0500

    A3: user can upload photo using createFileInput()

--- exercises/spy-profile-generator/js/script.js
+++ exercises/spy-profile-generator/js/script.js
@@ -2,10 +2,10 @@
 
 /*****************
 
-Spy Profile activity
-Author Name
+Spy Profile Generator
+Lydia
 
-generates a randomized spy profile for the user, and weakness protects it
+generates a randomized spy profile for the user
 
 Uses:
 Darius Kazemi's corpora project:
@@ -40,8 +40,6 @@ let spyProfile = {
   weakness: `**REDACTED**`,
 };
 
-let img;
-
 // User input variables
 let nameInput;
 // The buttons
@@ -53,7 +51,10 @@ let skillsButton;
 let weaknessButton;
 let disguiseButton;
 let randomProfileButton;
-
+//default spy photo
+let img;
+//user input photo
+let fileInput;
 
 // Variables to store JSON data for generating the profile
 let cannabisData;
@@ -88,23 +89,24 @@ function preload() {
   wineDescriptionsData = loadJSON(WINE_DESCRIPTIONS_DATA_URL);
   diseaseSymptomsData = loadJSON(DISEASE_SYMPTOMS_DATA_URL);
 
+  // load the default spy image
   img = loadImage("assets/images/spy.jpeg");
 }
 
 /**
-Creates a canvas then handles loading profile data, checking weakness,
-and generating a profile as necessary.
+Creates a canvas then creates buttons and inputs
 */
 function setup() {
   var canvas = createCanvas(425, 550);
-
-
 
   createSpan("What's your name? "); //label for name input
   nameInput = createInput();
   //nameInput.position(static);
   nameInput.changed(nameCallback);
   // nameInput.addClass('menu');
+
+  createSpan("Choose a photo for your spy profile!"); //label for file input
+  fileInput = createFileInput(handleFile);
 
   randomProfileButton = createButton("Randomize All");
   randomProfileButton.mousePressed(generateSpyProfile);
@@ -145,12 +147,26 @@ allows the name displayed to change when there is a new input
 */
 function nameCallback() {
   spyProfile.name = nameInput.value();
-  }
+}
 
-  /**
+/**
+handle the file input
+*/
+function handleFile(file) {
+  print(file);
+  if (file.type === 'image') {
+    img = createImg(file.data, '');
+    img.hide();
+  } else {
+    img = loadImage("assets/images/spy.jpeg");
+  }
+}
+
+
+/**
   Generates a new mission name from a cannabis name JSON and capializes it
   */
-function newMission(){
+function newMission() {
   let cannabis = random(cannabisData.cannabis);
   spyProfile.mission = cannabis.toUpperCase();
 }
@@ -158,59 +174,77 @@ function newMission(){
 /**
 Generates a new alias and capitalizes the first letter of each word
 */
-function newAlias(){
+function newAlias() {
   let instrument = random(instrumentsData.instruments);
   let wineDescription = random(wineDescriptionsData.wine_descriptions);
-  let statesOfDrunkenness = random(statesOfDrunkennessData.states_of_drunkenness);
-  let desciption = [capitalizeFirstLetter(wineDescription), capitalizeFirstLetter(statesOfDrunkenness)];
-  spyProfile.alias = `The \"${random(desciption)}\" ${capitalizeFirstLetter(instrument)}`;
+  let statesOfDrunkenness = random(
+    statesOfDrunkennessData.states_of_drunkenness
+  );
+  let desciption = [
+    capitalizeFirstLetter(wineDescription),
+    capitalizeFirstLetter(statesOfDrunkenness),
+  ];
+  spyProfile.alias = `The \"${random(desciption)}\" ${capitalizeFirstLetter(
+    instrument
+  )}`;
 }
 
 /**
 Generates a new random description with a strenth and an adverb
 */
-function newDescription(){
+function newDescription() {
   let adverb = random(adverbData.adverbs);
   let strength = random(encouragingWordsData.encouraging_words);
   let description = random(descriptionsData.descriptions);
-  spyProfile.desciption = `${capitalizeFirstLetter(description)} and ${adverb} ${strength}.`;
+  spyProfile.desciption = `${capitalizeFirstLetter(
+    description
+  )} and ${adverb} ${strength}.`;
 }
 
 /**
 Generates a new random weapon and capitalizes the first letter
 */
-function newSecretWeapon(){
+function newSecretWeapon() {
   let melee = random(weaponData.data.melee);
   let ranged = random(weaponData.data.ranged);
   let weapons = [capitalizeFirstLetter(melee), capitalizeFirstLetter(ranged)];
   spyProfile.secretWeapon = random(weapons);
 }
 
-function newDiguise(){
+/**
+Generates a new disguise
+*/
+function newDiguise() {
   let colors = random(colorData.colors);
   let color = colors.color;
   let clothing = random(clothingData.clothes);
   spyProfile.disguise = `${capitalizeFirstLetter(color)} ${clothing}`;
 }
 
-function newSkills(){
+/**
+Generates a new special skill
+*/
+function newSkills() {
   let card = random(tarotData.tarot_interpretations);
   let light = random(card.meanings.light);
   let subject = random(academicSubjects.subjects);
   spyProfile.skills = `${light}, and ${subject}.`;
 }
 
-function newWeakness(){
+/**
+Generates a new weakness
+*/
+function newWeakness() {
   let card = random(tarotData.tarot_interpretations);
   let shadow = random(card.meanings.shadow);
   let diseaseSymptom = random(diseaseSymptomsData.symptoms);
-  spyProfile.weakness = `${shadow}, and ${diseaseSymptom}`;
+  spyProfile.weakness = `${shadow}, and ${diseaseSymptom}.`;
 }
 
 /**
 Captialize the first letter in the given string
 */
-function capitalizeFirstLetter(string){
+function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
@@ -226,9 +260,6 @@ function generateSpyProfile() {
   newDiguise();
   newWeakness();
   newSkills();
-
-  // // saves the resulting profile to local storage
-  // localStorage.setItem(`spy-profile-data`,JSON.stringify(spyProfile));
 }
 
 /**
@@ -237,50 +268,62 @@ Displays the current spy profile.
 function draw() {
   background(255, 20, 150);
 
-push();
-blendMode(ADD);
-image(img, width - 210, 65, 200, 200);
-pop();
-
-
 // Generate the profile as a string using the data
-  let spyText = `Agent Name: ${spyProfile.name}
+let spyText = `Agent Name: ${spyProfile.name}
 
 Description: ${spyProfile.desciption}
 
 Secret Weapon: ${spyProfile.secretWeapon}
 
-Disguise: ${spyProfile.disguise}`
-
-
+Disguise: ${spyProfile.disguise}`;
 
   // Display the profile
   push();
-
-
-
   fill(255);
   noStroke();
-  rect(10, 10, width - 20, 45)
-  rect(10, height - 110, width - 20, 100 )
-  rect(10, height - 200, width - 20, 80 )
+  //text boxes
+
+  rect(10, 10, width - 20, 45);
+  rect(10, height - 110, width - 20, 100);
+  rect(10, height - 200, width - 20, 80);
 
   // the text
   textFont(`Courier, monospace`);
   textSize(16);
-
   fill(0);
   textAlign(CENTER, TOP);
-  text(`** TOP SECRET SPY PROFILE **`, width/2 , 15);
-  text(`** MISSION ${spyProfile.mission} **`, width/2, 35);
-  text(`Alias: `, width / 4, 275, width);
-  text(`${spyProfile.alias}`, width / 2, 295, 195);
-
+  text(`** TOP SECRET SPY PROFILE **`, width / 2, 15);
+  text(`** MISSION ${spyProfile.mission} **`, width / 2, 35);
+  text(`Alias: `, width / 4, 280, width);
+  text(`${spyProfile.alias}`, width / 2, 300, 195);
   textAlign(LEFT, TOP);
-
   text(spyText, 15, 70, 200);
-  text(`Special Skills: ${spyProfile.skills}`, 15, height - 190, width - 15, height);
-  text(`Weaknesses: ${spyProfile.weakness}`, 15, height - 100, width - 15, height);
-
+  text(
+    `Special Skills: ${spyProfile.skills}`,
+    15,
+    height - 190,
+    width - 15,
+    height
+  );
+  text(
+    `Weaknesses: ${spyProfile.weakness}`,
+    15,
+    height - 100,
+    width - 15,
+    height
+  );
   pop();
+
+  //the image
+  push();
+
+  fill(255, 255, 255, 190);
+  stroke(255);
+  strokeWeight(5)
+  rect(width - 213, 65, 200, 200);
+  blendMode(OVERLAY);
+  if (img){
+    image(img, width - 213, 65, 200, 200);
+  }
+pop();
 }
