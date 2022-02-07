493593aece854a07a7a69255139be445de49907c
Lydia <ldgravy@gmail.com>
2022-02-07 16:32:27 -0500

    A4: draw a pin

--- activities/bubble-popper/js/script.js
+++ activities/bubble-popper/js/script.js
@@ -21,6 +21,20 @@ let handpose;
 // The current set of predictions made by Handpose once it's running
 let predictions = [];
 
+// The bubble we will be popping
+let bubble;
+// The pin
+let pin = {
+  tip: {
+    x: undefined,
+    y: undefined,
+  },
+  head: {
+    x: undefined,
+    y: undefined,
+    size: 20,
+  },
+};
 
 /**
 Description of setup
@@ -31,28 +45,64 @@ function setup() {
   // Start webcam and hide the resulting HTML element
   video = createCapture(VIDEO);
   video.hide();
-  //video.position(0, 0);
-
+  video.position(0, 0);
 
   // Load the Handpose model
- handpose = ml5.handpose(video, {
-   flipHorizontal: true
- }, function() {
-    console.log(`Model loaded.`);
- });
+  handpose = ml5.handpose(
+    video,
+    {
+      flipHorizontal: true,
+    },
+    function () {
+      console.log(`Model loaded.`);
+    }
+  );
 
- // Listen for predictions
- handpose.on(`predict`, function(results){
-   console.log(results);
-   predictions = results;
- });
+  // Listen for predictions
+  handpose.on(`predict`, function (results) {
+    // console.log(results);
+    predictions = results;
+  });
+}
 
+function draw() {
+  background(0);
 
+  // Check if there currently predictions to display
+  if (predictions.length > 0) {
+    // If yes, then get the positions of the tip and base of the index finger
+    updatePin(predictions[0]);
+
+    // Display the current position of the pin
+    displayPin();
+  }
 }
 
 /**
-Description of draw()
+Updates the position of the pin according to the latest prediction
 */
-function draw() {
+function updatePin(prediction) {
+  pin.tip.x = prediction.annotations.indexFinger[3][0];
+  pin.tip.y = prediction.annotations.indexFinger[3][1];
+  pin.head.x = prediction.annotations.indexFinger[0][0];
+  pin.head.y = prediction.annotations.indexFinger[0][1];
+}
 
+/**
+Displays the pin based on the tip and base coordinates. Draws
+a line between them and adds a red pinhead.
+*/
+function displayPin() {
+  push();
+  noFill();
+  stroke(255);
+  fill(255);
+  line(pin.tip.x, pin.tip.y, pin.head.x, pin.head.y);
+  pop();
+
+  push();
+  noStroke();
+  fill(255, 0, 0);
+  ellipse(pin.head.x, pin.head.y, pin.head.size);
+  pop();
 }
