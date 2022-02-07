e2521fac3936594a86a6847dcc4fe3d179dd850d
Lydia <ldgravy@gmail.com>
2022-02-07 16:13:05 -0500

    A4: Get handpose working

--- activities/bubble-popper/js/script.js
+++ activities/bubble-popper/js/script.js
@@ -12,22 +12,43 @@ https://learn.ml5js.org/#/reference/handpose
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
+//the user's webcam
+let video = undefined;
+// The name of our model
+let modelName = `Handpose`;
+// Handpose object (using the name of the model for clarity)
+let handpose;
+// The current set of predictions made by Handpose once it's running
+let predictions = [];
 
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // Start webcam and hide the resulting HTML element
+  video = createCapture(VIDEO);
+  video.hide();
+  //video.position(0, 0);
+
+
+  // Load the Handpose model
+ handpose = ml5.handpose(video, {
+   flipHorizontal: true
+ }, function() {
+    console.log(`Model loaded.`);
+ });
+
+ // Listen for predictions
+ handpose.on(`predict`, function(results){
+   console.log(results);
+   predictions = results;
+ });
+
 
 }
-
 
 /**
 Description of draw()
