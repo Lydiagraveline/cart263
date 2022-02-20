3a63b63c37bf6ce9c22546a4673b7798bb13d292
Lydia <ldgravy@gmail.com>
2022-02-20 13:24:42 -0500

    Project 1: started code

---
+++ projects/baseline-test/js/script.js
@@ -0,0 +1,33 @@
+/**
+Baseline Test
+Lydia Graveline
+
+Program inspired by the Baseline Test from Bladerunner, a test designed to detect
+unwanted emotional responses in a "replicant."
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
