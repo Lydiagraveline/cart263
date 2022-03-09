1336d2488f3c4d4f47fc05d9bb933cacc0714671
Lydia <ldgravy@gmail.com>
2022-03-09 17:47:35 -0500

    E5: made poem start to disintegrate

---
+++ exercises/fragments-of-sappho/js/script.js
@@ -0,0 +1,30 @@
+/**
+Fragments of Sapho
+Lydia Graveline
+
+A webpage of ancient greek poetry by Sappho that is quickly disintegrating,
+it is the user's job to recover the fragmented poems by clicking on the missing
+lines. This program is based on Anne Carson's translation of Sappho "If Not, Winter".
+
+*/
+
+"use strict";
+
+setInterval(update, 500);
+
+/**
+Update is called every 500 milliseconds and it updates all the secrets on the page
+using jQuery`s each() function which calls the specified function on _each_ of the
+elements in the selection
+*/
+function update() {
+  $(`.recovered`).each(disintegrate);
+}
+
+function disintegrate(){
+  let r = Math.random();
+  if (r < 0.1) {
+    $(this).removeClass(`recovered`);
+    $(this).addClass(`redacted`);
+  }
+}
