7d7a5518c2b2142ce76a53ea427b7451e8d98a04
Lydia <ldgravy@gmail.com>
2022-03-22 19:16:09 -0400

    Project 2: started code

---
+++ projects/Project 2 Proposal/js/script.js
@@ -0,0 +1,33 @@
+/**
+Project 2 Proposal - Coral Grief
+Lydia Graveline
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
